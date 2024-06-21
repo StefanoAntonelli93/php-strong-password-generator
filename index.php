12400c5155f88682aa7c1ca530ff146b14d27c39
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-06-21 18:02:09 +0200

    [UPD] (milestone-2) create my-function.php and i added the function inside it.

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 // logica php
+
+// importo qui funzione generatePassword
+
+require_once __DIR__ . '/my-function.php';
+
+
 // se input form è popolato allora stampa 
 if (!empty($_GET['password_length'])) {
     $password_length = $_GET['password_length'];
@@ -8,24 +14,6 @@ if (!empty($_GET['password_length'])) {
 
 // creo funzione genera password
 
-function generatePassword($pw_length)
-{
-
-    $password = '';
-    // creo variabili range per lettere numeri e simboli
-    $upper_chars = range('A', 'Z');
-    $lower_chars = range('a', 'z');
-    $numbers = range(0, 9);
-    $symbols = range('!', '/');
-    // prendo tutti gli elementi degli array con spread operator
-    $password_chars = [...$upper_chars, ...$lower_chars, ...$numbers, ...$symbols];
-
-    for ($i = 0; $i < $pw_length; $i++) {
-        $rand = rand(0, count($password_chars) - 1);
-        $password .= $password_chars[$rand];
-    }
-    return $password;
-}
 
 
 
---
+++ my-function.php
@@ -0,0 +1,21 @@
+<?php
+
+
+function generatePassword($pw_length)
+{
+
+    $password = '';
+    // creo variabili range per lettere numeri e simboli
+    $upper_chars = range('A', 'Z');
+    $lower_chars = range('a', 'z');
+    $numbers = range(0, 9);
+    $symbols = range('!', '/');
+    // prendo tutti gli elementi degli array con spread operator
+    $password_chars = [...$upper_chars, ...$lower_chars, ...$numbers, ...$symbols];
+
+    for ($i = 0; $i < $pw_length; $i++) {
+        $rand = rand(0, count($password_chars) - 1);
+        $password .= $password_chars[$rand];
+    }
+    return $password;
+}
